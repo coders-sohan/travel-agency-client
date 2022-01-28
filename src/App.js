0301af65abc00e5c2d69d48bd2e990a1bfc22670
coders-sohan <coders.sohan.0216@gmail.com>
2022-01-28 17:28:41 +0600

    start coding and organize files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
 	return (
-		<div>
+		<div className="text-3xl underline decoration-wavy underline-offset-2 decoration-sky-500 text-sky-500 decoration-2">
 			<h1>Hello World!</h1>
 		</div>
 	);
